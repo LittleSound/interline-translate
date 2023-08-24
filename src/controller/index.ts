62aa249dd7c1b74cae2ca55822ef52401839bd53
LittleSound <464388324@qq.com>
2023-08-24 14:23:57 +0800

    fix: the selected area of translation

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,6 +1,8 @@
 import { RegisterTranslator } from './immersive'
+import { RegisterTranslateSelectedText } from './translateSelected'
 import type { Context } from '~/context'
 
 export function RegisterControllers(ctx: Context) {
   RegisterTranslator(ctx)
+  RegisterTranslateSelectedText(ctx)
 }
