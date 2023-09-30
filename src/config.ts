ec1fb180b3abd1886bf44f043866288d5389ece7
Rizumu Ayaka <rizumu@ayaka.moe>
2023-09-30 21:08:42 +0800

    feat: sync config in time

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import type { WritableComputedRef } from '@vue/reactivity'
 import { computed, reactive, ref } from '@vue/reactivity'
 import { Uri, workspace } from 'vscode'
 import { EXT_NAMESPACE } from './meta'
@@ -19,16 +20,19 @@ async function setConfig(key: string, value: any, isGlobal = true) {
     .update(key, value, isGlobal)
 }
 
-function createConfigRef<T>(key: string, defaultValue: T, isGlobal = true) {
-  return computed({
+function createConfigRef<T>(key: string, defaultValue: T, isGlobal = true): WritableComputedRef<T> {
+  const state = ref<T | undefined>(defaultValue)
+
+  return computed<T>({
     get: () => {
       // to force computed update
       // eslint-disable-next-line no-unused-expressions
       _configState.value
-      return getConfig<T>(key) ?? defaultValue
+      return state.value as T ?? getConfig<T>(key) ?? defaultValue
     },
-    set: (v) => {
-      setConfig(key, v, isGlobal)
+    set: (v: any) => {
+      state.value = v as any
+      setConfig(key, v, isGlobal).finally(() => state.value = undefined)
     },
   })
 }
