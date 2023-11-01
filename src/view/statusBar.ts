b51224ba13c0d4b1e67a869881dfb5877a3d0f7c
Rizumu Ayaka <rizumu@ayaka.moe>
2023-11-01 19:25:37 +0800

    feat: status bar show the translation status

--- src/view/statusBar.ts
+++ src/view/statusBar.ts
@@ -1,12 +1,22 @@
+import { effect } from '@vue/reactivity'
 import type { StatusBarItem } from 'vscode'
 import { StatusBarAlignment, window } from 'vscode'
+import type { Context } from '~/context'
+import { useStore } from '~/store'
 
 let entryButton: StatusBarItem
 
-export function registerEntryButton() {
+export function registerEntryButton(ctx: Context) {
   entryButton = window.createStatusBarItem(StatusBarAlignment.Right, 0)
   // TODO: need product icons
-  entryButton.text = '$(run-all) Translate'
-  entryButton.command = 'sidecar-translate.showTranslatePopmenu'
-  entryButton.show()
+  const store = useStore(ctx)
+  effect(() => {
+    entryButton.text = store.enableContinuousTranslation
+      ? store.callingTranslateService
+        ? '$(sync~spin) Translating'
+        : '$(pass) translated'
+      : '$(run-all) Translate'
+    entryButton.command = 'sidecar-translate.showTranslatePopmenu'
+    entryButton.show()
+  })
 }
