3b703ad8295208ed8fae4c695dad83d2138717d3
Rizumu Ayaka <rizumu@ayaka.moe>
2024-07-10 17:50:17 +0800

    chore: rename sidecar to interline

--- src/meta.ts
+++ src/meta.ts
@@ -1,3 +1,3 @@
 export const EXT_NAMESPACE = 'interline-translate'
 export const EXT_ID = 'rizumu.interline-translate'
-export const EXT_NAME = 'Sidecar Translate'
+export const EXT_NAME = 'Interline Translate'
